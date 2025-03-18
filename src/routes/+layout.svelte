93a242bf4e5318fe43d45c614b56643e38ee8bea
Navtoj Chahal <github@navtoj.com>
2025-03-18 05:05:02 -0700

    Track System Theme

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<ModeWatcher themeColors={{ dark: 'black', light: 'white' }} />
+
+{@render children()}
